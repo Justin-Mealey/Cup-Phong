2c7475ca615848c14effcac981075f6b2955beed
Justin Mealey <justinmealey16@gmail.com>
2024-11-09 15:27:58 -0800

    newlines to README, basic box example

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 # Cup Phong
 
 ## Setup:
-Run the following commands in your terminal:
-`git clone https://github.com/Justin-Mealey/Cup-Phong`
-`npm init -y`
-`npm install --save three`
+
+Run the following commands in your terminal:\
+`git clone https://github.com/Justin-Mealey/Cup-Phong`\
+`npm init -y`\
+`npm install --save three`\
 `npm install --save-dev vite`
 
-Now you can run the project with:
-`npx vite`
+Now you can run the project with:\
+`npx vite`\
 click on the local host link to open the game in your browser of choice
 
-If there are any issues:
-Check your installed version of npm and node (`npm -v` and `node -v`), update as necessary
+If there are any issues:\
+Check your installed version of npm and node (`npm -v` and `node -v`), update as necessary\
 Run `npm install`
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0,0,5)
+camera.position.set(0,0,20)
 camera.lookAt(0,0,0)
 
 const renderer = new THREE.WebGLRenderer();
@@ -20,20 +20,27 @@ const ballMaterial = new THREE.MeshBasicMaterial({
 const ball = new THREE.Mesh( ballGeometry, ballMaterial );
 scene.add( ball );
 
+//For development, remove later
 function createAxisLine(color, start, end) {
     const geometry = new THREE.BufferGeometry().setFromPoints([start, end]);
     const material = new THREE.LineBasicMaterial({ color: color });
     return new THREE.Line(geometry, material);
 }
-
-//For development, remove later
-const xAxis = createAxisLine(0xff0000, new THREE.Vector3(0, 0, 0), new THREE.Vector3(5, 0, 0)); // Red
-const yAxis = createAxisLine(0x00ff00, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 5, 0)); // Green
-const zAxis = createAxisLine(0x0000ff, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 5)); // Blue
+const xAxis = createAxisLine(0xff0000, new THREE.Vector3(0, 0, 0), new THREE.Vector3(20, 0, 0)); // Red
+const yAxis = createAxisLine(0x00ff00, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 20, 0)); // Green
+const zAxis = createAxisLine(0x0000ff, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 20)); // Blue
 scene.add(xAxis);
 scene.add(yAxis);
 scene.add(zAxis);
 //For development, remove later
+
+const b1g = new THREE.BoxGeometry(4, 0.5, 2); // width = 4, height = 1, depth = 2
+const b1m = new THREE.MeshBasicMaterial({ color: 0x0096FF, wireframe: false });
+const b1 = new THREE.Mesh(b1g, b1m);
+
+scene.add(b1)
+b1.position.set(2, 0, 0)
+
 
 function animate() {
 	renderer.render( scene, camera );
