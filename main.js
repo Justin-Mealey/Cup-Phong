4f517c39f7dc8548a73403d632f28b0cbe02e009
Justin Mealey <justinmealey16@gmail.com>
2024-11-09 14:30:10 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0,0,5)
+camera.lookAt(0,0,0)
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const ballGeometry = new THREE.SphereGeometry(1, 64, 64)
+const ballMaterial = new THREE.MeshBasicMaterial({
+    color: new THREE.Color(0xFFFFFF),
+})
+const ball = new THREE.Mesh( ballGeometry, ballMaterial );
+scene.add( ball );
+
+function createAxisLine(color, start, end) {
+    const geometry = new THREE.BufferGeometry().setFromPoints([start, end]);
+    const material = new THREE.LineBasicMaterial({ color: color });
+    return new THREE.Line(geometry, material);
+}
+
+//For development, remove later
+const xAxis = createAxisLine(0xff0000, new THREE.Vector3(0, 0, 0), new THREE.Vector3(5, 0, 0)); // Red
+const yAxis = createAxisLine(0x00ff00, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 5, 0)); // Green
+const zAxis = createAxisLine(0x0000ff, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 5)); // Blue
+scene.add(xAxis);
+scene.add(yAxis);
+scene.add(zAxis);
+//For development, remove later
+
+function animate() {
+	renderer.render( scene, camera );
+    controls.update(); 
+}
