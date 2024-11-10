16302b4c1d5663909b208fb3ee084fc19d0202c6
Brayden Tam <57848434+braydentam@users.noreply.github.com>
2024-11-10 15:19:29 -0800

    documentation for physics

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { createBall } from './ball.js';
-import { createPlanes } from './plane.js';
-import { translationMatrix, didCollide, updateVelocity } from './math.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -29,71 +26,7 @@ scene.add(yAxis);
 scene.add(zAxis);
 
 
-const ballRadius = 1;
-let ballPosition = new THREE.Vector3(0, 0, 0);
-let ballVelocity = new THREE.Vector3(-1, -0.2, 0);
-const ball = createBall(ballRadius, ballPosition);
-scene.add(ball);
-
-const planeData = [
-    // Left face
-    {
-        position: { x: -50, y: 0, z: 0 },
-        rotation: { x: 0, y: Math.PI / 2, z: 0 },
-        scale: { x: 1, y: 1, z: 1 },
-        horizontal: false
-    },
-
-    // Right face
-    {
-        position: { x: 50, y: 0, z: 0 },
-        rotation: { x: 0, y: -Math.PI / 2, z: 0 },
-        scale: { x: 1, y: 1, z: 1 },
-        horizontal: false
-    },
-
-    // Top face
-    {
-        position: { x: 0, y: 50, z: 0 },
-        rotation: { x: Math.PI / 2, y: 0, z: 0 },
-        scale: { x: 1, y: 1, z: 1 },
-        horizontal: true
-    },
-
-    // Bottom face
-    {
-        position: { x: 0, y: -50, z: 0 },
-        rotation: { x: -Math.PI / 2, y: 0, z: 0 },
-        scale: { x: 1, y: 1, z: 1 },
-        horizontal: true
-    },
-];
-
-const planes = createPlanes(planeData);
-planes.forEach(plane => scene.add(plane));
-
-function applyTranslation(ball, tx, ty, tz) {
-    const translation = translationMatrix(tx, ty, tz);
-    let model_transform = new THREE.Matrix4(); 
-    model_transform.multiplyMatrices(model_transform, translation) 
-    ball.matrix.copy(model_transform);
-    ball.matrixAutoUpdate = false;
-    ball.position.setFromMatrixPosition(ball.matrix);
-}
-
 function animate() {
-    const tx = ball.position.x + ballVelocity.x;
-    const ty = ball.position.y + ballVelocity.y;
-    const tz = ball.position.z + ballVelocity.z;
-
-    applyTranslation(ball, tx, ty, tz);
-
-    planeData.forEach((plane, index) => {
-        const hit_type = didCollide(ball, planeData[index]);
-        if (hit_type !== null) {
-            ballVelocity = updateVelocity(ball, ballRadius, ballVelocity, planeData[index], hit_type);
-        }
-    });
 
     // Render the scene
     renderer.render(scene, camera);
