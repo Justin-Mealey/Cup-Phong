0fe362a4d01402723fdb32fb50830521e26384b0
Daniel Guo <dguo314other@gmail.com>
2024-11-11 16:00:40 -0800

    Update main.js to match dev

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { setupStationaryObstacles } from './stationaryObstacles.js';
+import { setupEventListeners, final_power, final_angle} from './mouse.js'
+import { createBall } from './ball.js';
+import { translationMatrix, didCollide, updateVelocity } from './math.js';
+import { GAME_BALL_VELOCITY_SCALING_FACTOR } from './constants.js';
+import { createPlanes } from './plane.js'
+import { planeData } from './game_box.js';
+import { game_object } from './game_logic.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -14,6 +21,7 @@ document.body.appendChild( renderer.domElement );
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// For development, remove later
 function createAxisLine(color, start, end) {
     const geometry = new THREE.BufferGeometry().setFromPoints([start, end]);
     const material = new THREE.LineBasicMaterial({ color: color });
@@ -25,39 +33,91 @@ const zAxis = createAxisLine(0x0000ff, new THREE.Vector3(0, 0, 0), new THREE.Vec
 scene.add(xAxis);
 scene.add(yAxis);
 scene.add(zAxis);
-//For development, remove later
+// For development, remove later
 
+// Place stationary obstacles
 let obj = setupStationaryObstacles()
 let stationaryObstacles = obj.usedStationaryObstacles
 let floor = obj.floor
 let ceiling = obj.ceiling
-console.log(floor)
 
 let x = [7, 8.5, 11, 13.5, 16, 18.5, 21, 23.5]
 let y = [5, -4, 1, -2, 2, -4, 4, 0]
-let z = [0, 1, -1, 0, 2, 1, 0, -1]
-for (let i = 0; i < stationaryObstacles.length; i++){
-    let box = stationaryObstacles[i]
-    scene.add(box)
-    box.position.set(x[i], y[i], z[i])
-}
+//let z = [0, 2, -2, 0, 3, -1, 1, -1] ADD LATER when moving to 3D
+// for (let i = 0; i < stationaryObstacles.length; i++){
+//     let box = stationaryObstacles[i]
+//     scene.add(box)
+//     box.position.set(x[i], y[i], 0)
+// }
 
-scene.add(ceiling)
-ceiling.position.set(15, 8, 0)
-
-scene.add(floor)
-floor.position.set(15, -8, 0)
+const planes = createPlanes(planeData);
+planes.forEach(plane => scene.add(plane));
+// Place stationary obstacles
 
 //setup floor, ceiling
 //setup obstacles
 //setup cups
 //setup ball to be in start
 
+function applyTranslation(ball, tx, ty, tz) {
+    const translation = translationMatrix(tx, ty, tz);
+    let model_transform = new THREE.Matrix4(); 
+    model_transform.multiplyMatrices(model_transform, translation) 
+    ball.matrix.copy(model_transform);
+    ball.matrixAutoUpdate = false;
+    ball.position.setFromMatrixPosition(ball.matrix);
+}
+
+const ballRadius = 1;
+let ballPosition = new THREE.Vector3(0, 0, 0);
+const ball = createBall(ballRadius, ballPosition);
+scene.add(ball);
+
 setupEventListeners();
 
+// Camera event listener
+let cameraInTwoD = true
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'c') {
+      cameraInTwoD = !cameraInTwoD
+    }
+});
+// Camera event listener
+let setBallVelocity = false;
+let ballVelocity = new THREE.Vector3(0,0,0)
+
 function animate() {
+    if (cameraInTwoD){
+        let newPos = new THREE.Vector3(0, 0, 20);
+        camera.position.lerp(newPos, .08)
+        camera.lookAt(0,0,0)
+    }
+    else{
+        let newPos = new THREE.Vector3(-5, 0, 0);
+        camera.position.lerp(newPos, .08)
+        camera.lookAt(1,0,0)
+    }
+  
+    controls.enabled = false;
+    if(game_object.shot_ball == true && setBallVelocity == false){
+        ballVelocity.set(GAME_BALL_VELOCITY_SCALING_FACTOR * final_power * Math.cos(final_angle), GAME_BALL_VELOCITY_SCALING_FACTOR * final_power * Math.sin(final_angle), 0);
+        setBallVelocity = true;
+    }
+    else if(game_object.shot_ball == true && setBallVelocity == true){
+        const tx = ball.position.x + ballVelocity.x;
+        const ty = ball.position.y + ballVelocity.y;
+        const tz = ball.position.z + ballVelocity.z;
 
+        applyTranslation(ball, tx, ty, tz);
 
+        planeData.forEach((plane, index) => {
+            const hit_type = didCollide(ball, planeData[index]);
+            if (hit_type !== null) {
+                ballVelocity = updateVelocity(ball, ballRadius, ballVelocity, planeData[index], hit_type);
+            }
+        });
+    }
+    
     // Render the scene
     renderer.render(scene, camera);
     controls.update();
