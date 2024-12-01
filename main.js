88c91a1d732124d12fa74b6eabf41924e388dc32
Brayden Tam <57848434+braydentam@users.noreply.github.com>
2024-11-30 23:20:47 -0800

    added todo

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ import { createBackground } from './background.js';
 import { createPortals, getPortalBounds, checkTeleport } from './portals.js'
 
 
+//TODO: need to show animation on score/collission
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(50, 0, 40)
