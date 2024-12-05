ad192187212beb4d9cdbcb80624ada71a76b9136
Brayden Tam <57848434+braydentam@users.noreply.github.com>
2024-12-05 00:03:20 -0800

    added borders for z axis

--- game_box.js
+++ game_box.js
@@ -1,27 +1,27 @@
 import { GAME_BOUND_X } from "./constants";
 
 export const planeData = [
-    // // Left face
-    // {
-    //     position: { x: -50, y: 0, z: 0 },
-    //     rotation: { x: 0, y: Math.PI / 2, z: 0 },
-    //     scale: { x: 1, y: 1, z: 1 },
-    //     horizontal: false
-    // },
+    // Left face
+    {
+        position: { x: 50, y: 0, z: 10 },
+        rotation: { x: 0, y: 0, z: 0 },
+        scale: { x: GAME_BOUND_X, y: 32, z: 1 },
+        horizontal: false
+    },
 
-    // // Right face
-    // {
-    //     position: { x: 50, y: 0, z: 0 },
-    //     rotation: { x: 0, y: -Math.PI / 2, z: 0 },
-    //     scale: { x: 1, y: 1, z: 1 },
-    //     horizontal: false
-    // },
+    // Right face
+    {
+        position: { x: 50, y: 0, z: -10 },
+        rotation: { x: 0, y: 0, z: 0 },
+        scale: { x: GAME_BOUND_X, y: 32, z: 1 },
+        horizontal: false
+    },
 
     // Top face
     {
         position: { x: 50, y: 16, z: 0 },
         rotation: { x: Math.PI / 2, y: 0, z: 0 },
-        scale: { x: GAME_BOUND_X, y: 8, z: 1 },
+        scale: { x: GAME_BOUND_X, y: 20, z: 1 },
         horizontal: true
     },
 
@@ -29,7 +29,7 @@ export const planeData = [
     {
         position: { x: 50, y: -16, z: 0 },
         rotation: { x: -Math.PI / 2, y: 0, z: 0 },
-        scale: { x: GAME_BOUND_X, y: 8, z: 1 },
+        scale: { x: GAME_BOUND_X, y: 20, z: 1 },
         horizontal: true
     },
 ];
